7942ca9730e219248bc3181e2f0d1aa5555162da
Administrator <administrator@ubuntu.(none)>
2013-07-05 10:41:55 -0400

    updated

---
+++ app/controllers/work_controller.rb
@@ -0,0 +1,13 @@
+class WorkController < ApplicationController
+  def required
+  end
+
+  def faq
+  end
+
+  def articles
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfoApp::Application.config.secret_key_base = '8b8fbddb1883e9a6ccedaebdde68100c3e5176f71a7a544360cc3505a7a1ad65e56ec03322b675522f4382c04f58c649fb3a203aaf92aab3aab175f4aef13608'
